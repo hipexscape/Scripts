ed5aa7ca64cf549feeaf72c36028586afeec2e62
Abhinav <122148266+hipexscape@users.noreply.github.com>
2024-05-06 22:33:37 +0530

    hardware-clone: Track blobs from `xiaomi-mediatek-devs`

--- Clone/hardware-clone.sh
+++ Clone/hardware-clone.sh
@@ -4,6 +4,6 @@ rm -rf hardware/mediatek
 rm -rf hardware/xiaomi
 
 echo 'Cloning Updated hardware blobs'
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+git clone https://github.com/xiaomi-mediatek-devs/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
-git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
+git clone https://github.com/xiaomi-mediatek-devs/android_hardware_mediatek hardware/mediatek
